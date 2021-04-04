609eb42c7774871446f6ad0cd5bc864100bbcf68
Andrew <chen.andrew772@gmail.com>
2021-04-04 14:58:53 -0700

    add -notes to JS

--- JS/app.js
+++ JS/app.js
@@ -1,5 +1,10 @@
-// Button to change to a dark mode
+/* Button to change to a dark mode */
+// disables the css theme and enables another for colours
 
-// Add a new note and allow save and delete
+/* Add a new note and allow save and delete */
+// create a new note object or name
+//add it into the array after it is done and store what is written as a string in the object
 
-// Select notes
+/* Select notes */
+// Select from array
+// display from the array
