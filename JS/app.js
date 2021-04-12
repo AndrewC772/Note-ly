ecffde63f32c718aee7326fad70759c1bd47b1fd
Andrew <chen.andrew772@gmail.com>
2021-04-11 17:19:10 -0700

    ADD note recalling feature COMPLETE Dark_Mode changes

--- JS/app.js
+++ JS/app.js
@@ -2,20 +2,26 @@ let dark_mode_button = document.querySelector('.navdesign');
 let create_new_notes = document.querySelector('.notesbutton');
 let save_new_notes = document.querySelector('.buttonsave');
 let delete_new_notes = document.querySelector('.buttondelete');
+let test = document.querySelectorAll('.sidebaritems');
 /* Button to change to a dark mode */
 // disables the css theme and enables another for colours
 let colour_state = 'light'
 dark_mode_button.addEventListener('click', (ev)=>{
-    document.querySelector('.light_mode').classList.toggle("dark_mode");
-    document.querySelector('nav.light_mode').classList.toggle("dark_mode");
-    document.querySelector("button.light_mode").classList.toggle("dark_mode");
+    document.querySelector(".gridlayout").classList.toggle("dark_mode");
+    document.querySelector("nav.light_mode").classList.toggle("dark_mode");
+    document.querySelector(".notesbutton").classList.toggle("dark_mode");
+    document.querySelector(".buttonsave").classList.toggle("dark_mode");
+    document.querySelector(".buttondelete").classList.toggle("dark_mode");
+    document.querySelector(".navdesign").classList.toggle("dark_mode");
     document.querySelector(".sidemenuextend").classList.toggle("side_menu_extend_darkmode");
+    document.querySelector(".bottom").classList.toggle("dark_mode");
+    document.querySelector(".buttonextended").classList.toggle("side_menu_extend_darkmode");
     if (colour_state === 'light') {
-        document.querySelector("button.light_mode").innerHTML = "Light Mode"
+        document.querySelector(".navdesign").innerHTML = "Light Mode"
         console.log(colour_state)
         colour_state = 'dark'
     } else {
-        document.querySelector("button.light_mode").innerHTML = "Dark Mode"
+        document.querySelector(".navdesign").innerHTML = "Dark Mode"
         console.log(colour_state)
         colour_state = 'light'
     }
@@ -35,10 +41,42 @@ save_new_notes.addEventListener('click', ()=> {
     console.log('save a note')
     let Title_stored = document.querySelector('#notearea').firstChild.innerHTML;
     let entered_notes = convert_not_first()
+    let divs_stored = [...document.querySelectorAll('[contenteditable] > div:not(:first-child)')]
+    /* non-functional test code that failed to execute what i wanted :( */
     // let entered_notes = document.querySelector('#notearea').innerHTML
     // entered_notes.removeChild(entered_notes.childNodes[0]);
-    note_storing_array.push({Title_stored, entered_notes})
+    note_storing_array.push({Title_stored, entered_notes, divs_stored})
+    let new_list_item = document.createElement('li');
+    new_list_item.className = 'sidebaritems';
+    // new_list_item.onclick = recall_array_item(Title_stored)
+    let menu_title = document.createTextNode(`${Title_stored}`);
+    new_list_item.appendChild(menu_title);
+    document.querySelector('.Notes_stored').appendChild(new_list_item);
+    document.getElementById('notearea').outerHTML = '<div id="notearea" contenteditable="true">Enter title here</div>'
+    /*Failed Experiment 2 */
+    // for (let stored_note of note_storing_array) {
+    //     let { Title_stored } = stored_note;
+    //     test_backtick = `<li class="sidebaritems" > ${Title_stored} </li>`
+    //     document.querySelector('.Notes_stored').appendChild(test_backtick);
+    // }
+/* Select notes */
+// Select from array
+// display from the array
     console.log(note_storing_array)
+    let test = document.querySelectorAll('.sidebaritems');
+    test.forEach(item => {
+        item.addEventListener('click', (e) => {
+            let recall_title = e.currentTarget.textContent
+            console.log(recall_title)
+            for (let note_item of note_storing_array) {
+                let { Title_stored, entered_notes } = note_item;
+                if (Title_stored === recall_title){
+                    document.getElementById('notearea').outerHTML = `<div id="notearea" contenteditable="true">${Title_stored}<div>${entered_notes}</div></div>`
+                    console.log(entered_notes)
+                }
+            }
+        })
+    });
 });
 
 function convert_not_first() {
@@ -60,3 +98,26 @@ delete_new_notes.addEventListener('click', ()=> {
 /* Select notes */
 // Select from array
 // display from the array
+// function recall_array_item(Title_stored) {
+//     for (let note of note_storing_array) {
+//         let { title, body } = note;
+//         if (title === Title_stored){
+//             console.log(title, body)
+//         }
+//     }
+
+// test.forEach(item => {
+//     item.addEventListener('click', (e) => {
+//         let recall_title = e.currentTarget.textContent
+//         console.log(recall_title)
+         // for (let note of note_storing_array) {
+        //     console.log(note)
+            // let { title, body } = note;
+            // console.log(title, body)
+            // if (title == recall_title){
+            //     console.log(title, body) 
+            // } 
+        // }
+//     })
+// });
+    // document.getElementById('notearea').outerHTML = `<div id="notearea" contenteditable="true">Enter title here</div>`
